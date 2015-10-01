f118c7fecb981661fbc59e0aae7edf05c57a2cb5
ardhi95 <ar.dhi950@gmail.com>
2015-10-01 17:26:55 +0700

    Latihan
    Tugas merubah warna VB

---
+++ MI2D_1431140121_Ardhi_Nur_Fauzi/WindowsApplication7/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        If Me.BackColor = Color.Maroon Then
+            Me.BackColor = Color.Aqua
+        Else
+            Me.BackColor = Color.Maroon
+        End If
+    End Sub
+End Class
